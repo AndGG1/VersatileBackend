b7d7b03f2a37af7ff93e1e5f600de78f4f65d050
Andrei Grebla <andrei.greblaru@gmail.com>
2026-06-05 01:33:02 +0300

    feat(UserController): finished the Controller. Further ahead will be added: DELETE, UPDATE, GET for User DTO.

--- demo/src/main/kotlin/com/example/demo/User/structure/UserController.kt
+++ demo/src/main/kotlin/com/example/demo/User/structure/UserController.kt
@@ -1,6 +1,25 @@
 package com.example.demo.User.structure
 
-data class UserRequest(val username: String)
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
-class UserController {
+data class UserRequest(val username: String, val uid: String)
+
+@RestController
+@RequestMapping("/users")
+class UserController(private val userService: UserService) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createUser(@RequestBody request: UserRequest) : User {
+
+        return userService.createUser(
+            username = request.username,
+            uid = request.uid
+        )
+    }
 }
