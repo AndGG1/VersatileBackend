be0dec14fb72e18e9e1b4e63a4abbb505cedf552
Andrei Grebla <andrei.greblaru@gmail.com>
2026-08-21 23:03:43 +0300

    refactor(Gradle & XML): added some new dependencies

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -19,13 +19,13 @@ repositories {
 }
 
 dependencies {
-	//Redis Starters
+	// Redis Starters
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("redis.clients:jedis")
 
-	//Reactive Streams
-//	implementation("org.reactivestreams:reactive-streams")
-//	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	// Reactive Streams
+//  implementation("org.reactivestreams:reactive-streams")
+//  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
 	// Qdrant & gRPC Core
 	implementation("io.qdrant:client:1.11.0")
@@ -35,7 +35,7 @@ dependencies {
 
 	// Kotlin & Coroutines
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.9.0") // <-- Added right here! Provides the .await() function
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.9.0") // Provides the .await() function
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
 	// Spring Boot Starters
@@ -52,11 +52,12 @@ dependencies {
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
 	// Test Frameworks & Testcontainers
+	testImplementation(platform("org.testcontainers:testcontainers-bom:1.20.1"))
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.testcontainers:junit-jupiter")
-	testImplementation("org.testcontainers:qdrant:1.20.1")
+	testImplementation("org.testcontainers:qdrant")
 	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.12.2")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
